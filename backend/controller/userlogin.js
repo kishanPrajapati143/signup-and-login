056471a76049bb639a896418c7a68a23e0882a79
kishanPrajapati143 <kishanprajapati5896@gmail.com>
2025-09-27 19:22:29 +0530

    Update userlogin.js

--- backend/controller/userlogin.js
+++ backend/controller/userlogin.js
@@ -1,40 +1,4 @@
-// // const bcrypt = require('bcryptjs');
-// // const jwt = require('jsonwebtoken');
-// // const User = require('../models/user'); // Adjust path as needed
-// import jwt from 'jsonwebtoken';
 
-
-
-// // POST /login
-// const login = async (req, res) => {
-//     const { email, password } = req.body;
-//     try {
-//         // Find user by email
-//         const user = await User.findOne({ email });
-//         if (!user) {
-//             return res.status(401).json({ message: 'Invalid credentials' });
-//         }
-
-//         // Compare password
-//         const isMatch = await compare(password, user.password);
-//         if (!isMatch) {
-//             return res.status(401).json({ message: 'Invalid credentials' });
-//         }
-
-//         // Generate JWT token
-//         const token = jwt.sign(
-//             { userId: user._id, email: user.email },
-//             process.env.JWT_SECRET || 'yoursecretkey',
-//             { expiresIn: '1h' }
-//         );
-
-//         res.json({ token, userId: user._id, email: user.email });
-//     } catch (err) {
-//         res.status(500).json({ message: 'Server error' });
-//     }
-// };
-
-// export default login;
 import jwt from 'jsonwebtoken';
 import user from "../models/usermodel.js";
 
